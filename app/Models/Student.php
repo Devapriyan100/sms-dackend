b9267f733cf7d30faad5c1d0ac9f3551afb6fc96
Deva <devachandar2000@gmain.com>
2025-07-06 19:34:50 +0530

    added db, relationship, factories, seeders

---
+++ app/Models/Student.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    /** @use HasFactory<\Database\Factories\StudentFactory> */
+    use HasFactory;
+
+    public function courses(){
+        return $this->belongsToMany(Course::class, 'enrollments')
+                    ->withTimestamps()
+                    ->withPivot('enrolled_at');
+    }
+}
